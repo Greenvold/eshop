852cbdf62cef882c4f60bc3c0389887041904723
Rudolf Bruder <rudolf.bruder@gmail.com>
2021-02-17 14:26:44 +0100

    Product  factory, seeder and migration

--- database/migrations/2021_02_17_131214_create_products_table.php
+++ database/migrations/2021_02_17_131214_create_products_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class CreateProductsTable extends Migration
 {
@@ -15,6 +16,9 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('unique_hash')->default(md5(Str::random(16)));
             $table->timestamps();
         });
     }
